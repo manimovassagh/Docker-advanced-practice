995854cf12204bcf84f7e4e5cf95dbc8f6bc6068
Mani Movassagh <mani.mgh@gmail.com>
2024-08-27 00:52:45 +0200

    docker

--- react/vite.config.ts
+++ react/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all IP addresses
+    port: 5174, // The port that Vite will use
+    strictPort: true, // Exit if the port is already in use
+  },
 })
