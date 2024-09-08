5be8954e8699bc7dd6f6374db5563918f72e9f8a
Mani Movassagh <mani.mgh@gmail.com>
2024-09-08 21:49:27 +0200

    foo-comp

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,9 @@
 ﻿
 using ConsoleApp1;
 
-Student student = new Student(1, "Mani Mova", 20, "mani@example.com", "123 Main St");
-Student student2 = new Student(2, "Sahar", 22, "mani@example.com", "123 Main St");
+Student student = new Student(1, "Mani Mova", 20, "mani@example.com", "123 Main St","foo");
+Student student2 = new Student(2, "Sahar", 22, "mani@example.com", "123 Main St","foo-company");
 
 Console.WriteLine(student.Name);
 Console.WriteLine(student2.Name);
+Console.WriteLine(student2.Company);
--- ConsoleApp1/Student.cs
+++ ConsoleApp1/Student.cs
@@ -10,15 +10,17 @@ namespace ConsoleApp1
         public int Age { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
+        public string Company { get; set; }
 
         // Constructor to initialize the Student entity
-        public Student(int id, string name, int age, string email, string address)
+        public Student(int id, string name, int age, string email, string address,string company)
         {
             Id = id;
             Name = name;
             Age = age;
             Email = email;
             Address = address;
+            Company = company;
         }
     }
 
