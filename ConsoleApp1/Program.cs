44d67e201c987c215d2ea1617c4b5ee70dbee3fd
Mani Movassagh <mani.mgh@gmail.com>
2024-09-08 23:51:33 +0200

    json serialiser

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,16 @@
-﻿
-using ConsoleApp1;
+﻿using ConsoleApp1;
 using ConsoleApp1.models;
+using System.Text.Json;
 
-Student student = new Student(1, "Mani Mova", 20, "mani@example.com", "123 Main St","foo");
-Student student2 = new Student(2, "Sahar", 22, "mani@example.com", "123 Main St","foo-company");
+Student student = new Student(1, "Mani Mova", 20, "mani@example.com", "123 Main St", "foo");
+Student student2 = new Student(2, "Sahar", 22, "mani@example.com", "123 Main St", "foo-company");
 Course course = new Course(1, "Learning dotnet c#");
 Mani mani = new();
-int res = mani.sum(4,12);
-string t =course.GetSampleCourse();
+int res = mani.sum(4, 12);
+string t = course.GetSampleCourse();
 Console.WriteLine(mani.CourserGiver().Name);
 Console.WriteLine(res);
 
+// Serialize student2 to JSON
+string student2Json = JsonSerializer.Serialize(student2, new JsonSerializerOptions { WriteIndented = true });
+Console.WriteLine(student2Json);
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4542c50e3274fa75c111668909be52c30e3a084")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f56ad24655886160b2a2316248cc8405a71503e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
