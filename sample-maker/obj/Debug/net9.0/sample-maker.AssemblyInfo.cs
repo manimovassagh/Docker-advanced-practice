be6d686e55d03be8276175e7ad2ddf48b1135a51
Mani Movassagh <mani.mgh@gmail.com>
2024-09-27 21:22:54 +0200

    docker

--- sample-maker/obj/Debug/net9.0/sample-maker.AssemblyInfo.cs
+++ sample-maker/obj/Debug/net9.0/sample-maker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sample-maker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0e9c774e2f5f898b45a5c1f7cb08ac5676b6cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec26c4097cf037ea0347da21cb3efb7d762584a")]
 [assembly: System.Reflection.AssemblyProductAttribute("sample-maker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sample-maker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
