e3466872037eefeed00ef2fec424c2e59216ce1a
Mani Movassagh <mani.mgh@gmail.com>
2024-09-27 12:17:26 +0200

    change container to multi layer

--- dotnet-docker/HelloWorldApi/obj/Debug/net8.0/HelloWorldApi.AssemblyInfo.cs
+++ dotnet-docker/HelloWorldApi/obj/Debug/net8.0/HelloWorldApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorldApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e909689da8b8f4e4233ab015eed3dd0a2fc6695")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d092d1381b840785e37e0e3470157dcdf0e03398")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorldApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorldApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
