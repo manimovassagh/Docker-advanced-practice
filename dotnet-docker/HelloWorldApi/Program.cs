9f5136ad03908745c2fe7dcfa7001f97eb1bcebd
Mani Movassagh <mani.mgh@gmail.com>
2024-10-18 02:31:54 +0200

    minimal api

--- dotnet-docker/HelloWorldApi/Program.cs
+++ dotnet-docker/HelloWorldApi/Program.cs
@@ -12,6 +12,9 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => new { Name = "Practice makes perfect 😄🚀", Age = 30 });
+
+
+
 app.MapGet("/del", DelHandler.DelHandleInit);
 
 // Map controller routes
