47621cd1cd6f7ed19cf0c369de5c0d8bf84a8285
Mani Movassagh <mani.mgh@gmail.com>
2024-11-09 19:49:44 +0100

    docker

--- dotnet-docker/HelloWorldApi/Program.cs
+++ dotnet-docker/HelloWorldApi/Program.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
+
 // Add services to the container.
 builder.Services.AddControllers(); // Register the controllers
 
